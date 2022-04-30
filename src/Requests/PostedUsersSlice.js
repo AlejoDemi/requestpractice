44d437ed9d399c24b1811a6af24dd8688e562cea
AlejoDemi <alejo.demitropulos@ing.austral.edu.ar>
2022-04-29 23:25:17 -0300

    attempt

---
+++ src/Requests/PostedUsersSlice.js
@@ -0,0 +1,12 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const initialState=[];
+
+const PostedUsersSlice=createSlice({
+    name:"postedUsers",
+    initialState,
+    reducers:{}
+
+})
+
+export default PostedUsersSlice.reducer;
